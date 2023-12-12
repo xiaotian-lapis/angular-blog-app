929d9ce4e0ce9c68f5e2e07791fe2d2d5729ae6f
xli <Xiaotian.Li@lapisit.com.au>
2023-12-12 12:03:53 +1100

    feat(basic-angular-blog-app): fixed marker and server behavior

--- src/app/pages/discover/discover.component.ts
+++ src/app/pages/discover/discover.component.ts
@@ -3,6 +3,8 @@ import {LeafletModule} from "@asymmetrik/ngx-leaflet";
 import {icon, latLng, Layer, marker, tileLayer} from "leaflet";
 import {Store} from "@ngrx/store";
 import {selectAllBlogs} from "../../state/selectors/blog.selector";
+import {BlogActions} from "../../state/actions/blog.action";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-discover',
@@ -17,14 +19,15 @@ export class DiscoverComponent implements OnInit {
 
   constructor(
     private store: Store,
+    private router: Router,
   ) {
   }
 
   options = {
     layers: [
       tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        { maxZoom: 18, attribution: '...' })
+        'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg',
+        {maxZoom: 18, attribution: '...'})
     ],
     zoom: 15,
     center: latLng(-37.81638808755261, 144.9566792258329)
@@ -36,24 +39,43 @@ export class DiscoverComponent implements OnInit {
   selectBlogs$ = this.store.select(selectAllBlogs);
 
   ngOnInit(): void {
+
+    // dispatch load action to load logs into store
+    this.store.dispatch(BlogActions.loadBlogs());
+
     this.selectBlogs$.subscribe(blogs => {
       console.log("blogs for discover page: ", blogs);
       this.layers = blogs.map(
         blog => {
-          return marker(
+          const popupContent =
+            `
+              <h3>${blog.title}</h3>
+              <strong>By: ${blog.author}</strong>
+              <hr>
+              <button id="view-blog-${blog.id}" class="leaflet-popup-button">View</button>
+            `;
+
+          const blogMarker = marker(
             [blog.location.lat, blog.location.lng],
             {
               title: blog.title,
               riseOnHover: true,
               icon: icon({
                 iconUrl: './assets/marker-icon.png',
-                iconSize: [30, 40]
+                iconSize: [20, 35]
               })
             }
-          ).bindPopup(`
-            <h3>${blog.title}</h3>
-            <a>View</a>`
-          );
+          ).bindPopup(popupContent);
+
+          // attach event listener to the button for jumping to blog page
+          blogMarker.on('popupopen', () => {
+            const button = document.getElementById(`view-blog-${blog.id}`);
+            button?.addEventListener('click', () => {
+              this.router.navigate(['/blog', blog.id]);
+            });
+          });
+
+          return blogMarker;
         }
       )
     });
