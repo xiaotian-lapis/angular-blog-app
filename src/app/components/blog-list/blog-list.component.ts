0213805f1530750bfd193c442e6caa96b44b71ef
xli <Xiaotian.Li@lapisit.com.au>
2023-12-19 11:22:48 +1100

    fix(basic-angular-blog-app): Action implementations to standard methods

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
-import {RouterLink} from '@angular/router';
-import {IBlog} from '../../shared/models/blog.model';
-import {Observable} from 'rxjs';
-import {selectAllBlogs, selectBlogsViewStatus,} from '../../state/selectors/blog.selector';
-import {Store} from '@ngrx/store';
-import {BlogActions} from '../../state/actions/blog.action';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {ViewStatus} from "../../shared/constants/status.constant";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {equals, or} from "../../shared/utils/ramda-functions.util";
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { IBlog } from '../../shared/models/blog.model';
+import { Observable } from 'rxjs';
+import {
+  selectAllBlogs,
+  selectBlogsViewStatus,
+} from '../../state/selectors/blog.selector';
+import { Store } from '@ngrx/store';
+import * as BlogActions from '../../state/actions/blog.action';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ViewStatus } from '../../shared/constants/status.constant';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { equals, or } from '../../shared/utils/ramda-functions.util';
 
 @Component({
   selector: 'app-blog-list',
@@ -32,14 +35,15 @@ import {equals, or} from "../../shared/utils/ramda-functions.util";
 export class BlogListComponent implements OnInit {
   blogList$: Observable<IBlog[]> = this.store.select(selectAllBlogs);
 
-  viewStatus$: Observable<ViewStatus> = this.store.select(selectBlogsViewStatus);
+  viewStatus$: Observable<ViewStatus> = this.store.select(
+    selectBlogsViewStatus,
+  );
   protected readonly ViewStatus = ViewStatus;
 
   protected readonly equals = equals;
   protected readonly or = or;
 
-  constructor(private store: Store) {
-  }
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
     // dispatch load action to load logs into store
@@ -54,7 +58,7 @@ export class BlogListComponent implements OnInit {
     this.store.dispatch(
       BlogActions.removeBlog({
         id: blogId,
-      })
+      }),
     );
   }
 }
--- src/app/state/actions/blog.action.ts
+++ src/app/state/actions/blog.action.ts
@@ -1,54 +1,55 @@
-import {createActionGroup, emptyProps, props} from '@ngrx/store';
-import {IBlog} from '../../shared/models/blog.model';
+import { createAction, props } from '@ngrx/store';
+import { IBlog } from '../../shared/models/blog.model';
 
 /**
  * Blog Actions
  */
-export const BlogActions = createActionGroup({
-  source: 'Blog',
-  events: {
-    'Load Blogs': emptyProps(),
-    'Add Blog': props<{
-      id: string;
-      author: string;
-      title: string;
-      description: string;
-      content: string;
-      createdTime: Date;
-      updatedTime: Date;
-      location: {
-        lat: number;
-        lng: number;
-        addr: string;
-      };
-    }>(),
-    'Remove Blog': props<{
-      id: string;
-    }>(),
-    'Update Blog': props<{
-      id: string;
-      author: string;
-      title: string;
-      description: string;
-      content: string;
-      createdTime: Date;
-      updatedTime: Date;
-      location: {
-        lat: number;
-        lng: number;
-        addr: string;
-      };
-    }>(),
-  },
-});
+export const loadBlogs = createAction('[Blog] Load Blogs');
+export const addBlog = createAction(
+  '[Blog] Add Blog',
+  props<{
+    id: string;
+    author: string;
+    title: string;
+    description: string;
+    content: string;
+    createdTime: Date;
+    updatedTime: Date;
+    location: {
+      lat: number;
+      lng: number;
+      addr: string;
+    };
+  }>(),
+);
+export const removeBlog = createAction(
+  '[Blog] Remove Blog',
+  props<{ id: string }>(),
+);
+export const updateBlog = createAction(
+  '[Blog] Update Blog',
+  props<{
+    id: string;
+    author: string;
+    title: string;
+    description: string;
+    content: string;
+    createdTime: Date;
+    updatedTime: Date;
+    location: {
+      lat: number;
+      lng: number;
+      addr: string;
+    };
+  }>(),
+);
 
-/**
- * Blog API Actions
- */
-export const BlogApiActions = createActionGroup({
-  source: 'Blog API',
-  events: {
-    'Blogs Loaded Success': props<{ blogs: IBlog[] | null }>(), // Action for successful blog load
-    'Blogs Loaded Error': props<{ error: { message: string } }>(), // Action for error in loading blogs
-  },
-});
+// Define each action for Blog API
+export const blogsLoadedSuccess = createAction(
+  '[Blog API] Blogs Loaded Success',
+  props<{ blogs: IBlog[] | null }>(),
+);
+export const blogsLoadedError = createAction(
+  '[Blog API] Blogs Loaded Error',
+  props<{ error: { message: string } }>(),
+);
